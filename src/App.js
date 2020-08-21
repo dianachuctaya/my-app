ec6a3a0c1bd7ee7d97c574df78bba6b156ef84a8
dianachuctaya <dianachuctayapacheco@gmail.com>
2020-08-20 22:22:25 -0500

    subiendo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SaludoNuevo from './Componets/Nuevosaludo';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Aprender React JS
+       </h1>
+       <SaludoNuevo />
       </header>
+         
     </div>
   );
 }
 
 export default App;
+{/* <div className="App">
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>
+      Editando Aqyi <code>src/App.js</code> ISEPHDE.
+        </p>
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn React
+        </a>
+  </header>
+</div> */}
---
+++ src/Componets/Nuevosaludo.jsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+const SaludoNuevo = () => {
+    return ( 
+        <Fragment>
+            <h2>Nuevo Saludo...</h2>
+            <h3>Publicando....</h3>
+            <h4>Nuevo Saludo...</h4>
+            <h3>Nuevo Saludo...</h3>
+            <h2>Nuevo Saludo...</h2>
+          
+            
+        </Fragment>
+            
+     );
+}
+ 
+export default SaludoNuevo;
